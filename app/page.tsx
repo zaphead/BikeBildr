eb49ef4673da95142cec5a2edca703c0eed27bfd
Spencer Terry <sterryyr@gmail.com>
2026-03-26 19:48:58 -0600

    gpt so booty at ui

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { BuildStudio } from "@/components/build-studio";
-
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-[var(--bg)] text-[var(--ink)]">
-      <BuildStudio />
-    </main>
-  );
-}
